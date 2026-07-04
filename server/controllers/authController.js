68f3d97c6ef5a8f31b2d9b49735a2429ade22ef0
Aman Kushwaha <amankushwahaa08@gmail.com>
2026-07-04 18:28:05 +0530

    register login and logout

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -42,7 +42,7 @@ export const register = async (req, res) => {
 
 }
 
-export const login = async () => {
+export const login = async (req, res) => {
     const {email, password} = req.body;
 
     if(!email || !password){
