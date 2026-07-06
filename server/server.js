7aad96384ce4ab2e7fee57c37a7a01d36b89e92a
Aman Kushwaha <amankushwahaa08@gmail.com>
2026-07-06 16:07:50 +0530

    return user details feature

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/authRoutes.js" 
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -18,4 +19,5 @@ app.listen(port, ()=> console.log(`Server stated on PORT:${port}`));
 //API Endpoint
 app.get('/', (req, res)=> res.send("API Working"))
 app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
 
